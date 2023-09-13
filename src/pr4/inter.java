48035cc877f4bcf4f6dbe8bb21f7b23c56f0cff3
Тимур <2ctoo13@gmail.com>
2023-09-13 21:36:53 +0300

    added pr4 updated

--- src/pr4/inter.java
+++ src/pr4/inter.java
@@ -1,5 +1,44 @@
 package pr4;
-
+import pr4.shop;
+import java.util.Scanner;
 public class inter {
-
+	public void start() {
+		shop Shop = new shop();
+		Scanner inp = new Scanner(System.in);
+		while(true) {
+			System.out.println("1: add pc\n2: delete pc\n3: find pc");
+			int choise = inp.nextInt();
+			switch(choise) {
+			case 1:
+			{
+				System.out.println("format : <price id> ");
+				double price = inp.nextDouble();
+				int id = inp.nextInt();
+				Shop.addToCatalog(new pc(id, price));
+				break;
+			}
+			case 2:
+			{
+				System.out.println("format : <id> ");
+				int id = inp.nextInt();
+				Shop.deleteFromCatalog(id);
+				break;
+			}
+			case 3:
+			{
+				System.out.println("format : <id> ");
+				int id = inp.nextInt();
+				try {
+					pc foundPc = Shop.searchProduct(id);
+					System.out.printf("Found\nid: %d\nprice: %f ", foundPc.getId(), foundPc.getPrice());
+				}
+				catch(Throwable t){
+					System.out.printf("Not found %d", id);
+				}
+				break;
+			}
+			}
+		}
+	}
+	
 }
--- src/pr4/shop.java
+++ src/pr4/shop.java
@@ -1,6 +1,28 @@
 package pr4;
 import pr4.pc;
-
+import java.util.ArrayList;
+import java.util.Scanner;
 public class shop {
+	private ArrayList<pc> catalog = new ArrayList<pc>();
+
+	public void addToCatalog(pc Pc) {
+		this.catalog.add(Pc);
+	}
+
+	public void deleteFromCatalog(int id) {
+		for (pc Now : this.catalog) {
+			if(Now.getId() == id) {
+				catalog.remove(Now);
+			}
+		}
+	}
+	public pc searchProduct(int id) {
+		for (pc Now : this.catalog) {
+			if(Now.getId() == id) {
+				return Now;
+			}
+		}
+		throw new Error();
+	}
 	
 }
